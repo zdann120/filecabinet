80961b780f80c0a86f950686f1f2ae26e50af73f
Zach Nusbaum <zachdnusbaum@gmail.com>
2016-04-11 07:42:22 +0000

    update view file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,12 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_KEY_ID'],                        # required
-    aws_secret_access_key: ENV['AWS_ACCESS_KEY'],                        # required
-  }
-  config.fog_directory  = 'znfilecabinet'                          # required
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+if !Rails.env.development?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV['AWS_KEY_ID'],                        # required
+      aws_secret_access_key: ENV['AWS_ACCESS_KEY'],                        # required
+    }
+    config.fog_directory  = 'znfilecabinet'                          # required
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+  end
 end
