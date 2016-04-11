ce32017833320779d31fe0f374f6c7adb648d9f8
Zach Nusbaum <zachdnusbaum@gmail.com>
2016-04-11 08:12:06 +0000

    add reconfirmable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411080451) do
+ActiveRecord::Schema.define(version: 20160411080910) do
 
   create_table "docs", force: :cascade do |t|
     t.string   "title"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20160411080451) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
